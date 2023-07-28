b8c4ead2ffc680a788c8ae6e0df0d20abb616a52
Clayton Zambon <zambon@outlook.com.br>
2023-07-28 19:15:38 +0000

    "FEAT: GitPod - HelloWorld"

---
+++ output/examples_C/HelloWorld.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("=================\n");
+    printf("==\n");
+    printf("Hello World\n");
+    printf("==\n");
+    printf("=================");
+
+    return 0;
+}
